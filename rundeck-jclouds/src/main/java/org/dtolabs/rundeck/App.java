9f54d528a108c138b79d24491da98582fdad3f67
Greg Schueler <greg.schueler@gmail.com>
2011-02-21 15:26:06 -0800

    Initial maven project

---
+++ rundeck-jclouds/src/main/java/org/dtolabs/rundeck/App.java
@@ -0,0 +1,13 @@
+package org.dtolabs.rundeck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
